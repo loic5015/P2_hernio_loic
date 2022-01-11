e33afb022b7fc10889b34ecc13f8974be9b13dc5
loic5015 <loic.hernio@gmail.com>
2022-01-11 12:13:02 +0100

    resolution d'un bug sur la pagination

--- scraper.py
+++ scraper.py
@@ -8,22 +8,27 @@ import math
 import re
 import os
 
-def compiler_fichier_csv(data, categorie, current_directory):
+
+def compiler_fichier_csv(data, categorie):
     entete = ["product_page_url", "universal_ product_code (upc)", "title",
               "price_including_tax", "price_excluding_tax", "number_available", "product_description",
               "category", "review_rating", "image_url"]
-    """if not os.path.exists(os.path.join(current_directory, "csv")):
-        os.mkdir(os.path.join(current_directory, "csv"))
-    directory_travail = os.path.join(current_directory, "csv\\"+categorie)
-    os.mkdir(directory_travail)
-    os.chdir(directory_travail)"""
+
     with open('resultat_scrapping_'+categorie+'.csv', 'w', encoding="utf-8", newline='') as file:
         writer = csv.writer(file, delimiter=',')
         writer.writerow(entete)
-        print(data)
         for row in data:
-            print(row)
             writer.writerow(row)
+
+def telecharger_images(data):
+    if not os.path.exists(os.path.join(current_directory, "images")):
+        os.mkdir(os.path.join(current_directory, "images"))
+    directory_travail = os.path.join(current_directory, "images")
+    for row in data:
+        nom_fichier = row[0].split('/')[4]
+        response = requests.get(row[9]).content
+        with open(directory_travail+"\\"+nom_fichier+".jpg", "wb+") as file:
+            file.write(response)
 
 
 
@@ -36,7 +41,6 @@ if response.ok:
     urls = soup.find('div', class_="side_categories").findAll('a')
     for url in urls:
         urls_a_scrapper.append(["http://books.toscrape.com/catalogue/category"+url['href'][2:], url.text.strip()])
-        print(urls_a_scrapper)
 
 for url_categorie in urls_a_scrapper[1:]:
     # scrap le nombre de livres et les urls de tous les livres
@@ -54,9 +58,13 @@ for url_categorie in urls_a_scrapper[1:]:
             livres.append(nombre_de_livres.text)
         if math.ceil(int(livres[0])/20) > 1:
             # met a jour la liste urls
-            urls.append("http://books.toscrape.com/catalogue/category/books/mystery_3/page-"+str(math.ceil(int(livres[0])/20))
-                        +".html")
-
+            liste_url = url_categorie[0].split('/')[:-1]
+            url_pagination = ("/").join(liste_url)
+            i = 2
+            while i <= math.ceil(int(livres[0])/20):
+                urls.append(url_pagination+"/page-"
+                            + str(i)+".html")
+                i = i + 1
         # rechercher toutes les urls des livres sur toutes les pages de la categorie
         for url_pagines in urls:
             response = requests.get(url_pagines)
@@ -73,21 +81,33 @@ for url_categorie in urls_a_scrapper[1:]:
             response = requests.get(url_livre)
             data_livre = []
             if response.ok:
+                #product_page_url
                 data_livre.append(url_livre)
                 soup = BeautifulSoup(response.content, features="html.parser")
                 tds = soup.findAll('td')
+                #universal_ product_code (upc)
                 data_livre.append(tds[0].text)
                 title = soup.find('h1')
+                #title
                 data_livre.append(title.text)
+                #price_including_tax
                 data_livre.append(tds[3].text)
+                #price_excluding_tax
                 data_livre.append(tds[2].text)
+                #number_available
                 livres_disponibles = re.findall("\d+", tds[5].text)
                 data_livre.append(livres_disponibles[0])
+                #product_description
                 description = soup.find(class_="product_page").findAll('p')
                 data_livre.append(description[3].text)
+                #category
                 data_livre.append(categorie)
+                #review_rating
                 data_livre.append(tds[6].text)
+                #image_url
                 image_img = soup.find(class_="carousel").find('img')
                 data_livre.append("http://books.toscrape.com" + image_img['src'][5:])
                 data_csv.append(data_livre)
-    compiler_fichier_csv(data_csv, categorie, current_directory)
+
+    compiler_fichier_csv(data_csv, categorie)
+    telecharger_images(data_csv)
